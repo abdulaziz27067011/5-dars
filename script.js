3dd458143aede81536a254714f7ece2c8f9455aa
abdulaziz27067011 <abdullox30007@gmail.com>
2025-02-17 13:59:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+let  = prompt(`ismingizni kiriting`)
+
+let student_data = {
+
+}
+
+let ism =prompt( `ismingizni kiriting`)
+let coin =prompt( `ismingizni kiriting`)
+let energy =prompt( `ismingizni kiriting`)
+let familya =prompt( `ismingizni kiriting`)
+
+
+student_data.name=ism
+student_data.coin= coin
+student_data.energy=energy
+student_data.familya=familya
+
+
+console.log(student_data);
+
+
+
+
+
+
+
+
+
